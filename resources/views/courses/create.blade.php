be1accf6c56630afafe176c437c4baa76312ef61
johned21 <johnpaulnunez14@gmail.com>
2020-12-17 22:21:51 -0800

    Finals

---
+++ resources/views/courses/create.blade.php
@@ -0,0 +1,36 @@
+@extends('base')
+
+@section('content')
+
+<br> 
+    <div class="container">
+        <div class="float-right">
+            <a href="{{url('/courses')}}" class="btn btn-primary btn-sm">
+                Back
+            </a>
+        </div>
+
+        <h2>Create New Courses</h2>
+
+        <div class="row">
+            <div class="col-md-5">
+            
+                {!!  Form::open(['url'=>'/courses', 'method'=>'post']) !!}
+
+                    @include('courses._form')
+
+                    <div class="form-group">
+                        <button class="btn btn-primary btn-sm float-right">Create Course</button>
+                    </div>
+
+                {!!  Form::close() !!}
+            
+            </div>
+
+            <div class="col-md-7">
+                @include('errors')
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
